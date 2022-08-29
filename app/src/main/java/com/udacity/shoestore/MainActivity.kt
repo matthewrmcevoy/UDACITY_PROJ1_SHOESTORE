32bdbc3144e2f4afc9e6c9eaf04b8bc4567c40bb
Matthew R McEvoy <matthewrmcevoy@gmail.com>
2022-08-29 10:00:25 -0400

    commit v4; overflow menu back to login navigation. Attempting to limit up navigation based on NavDestination

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.NavigationUI.navigateUp
@@ -11,7 +12,7 @@ import timber.log.Timber
 import com.udacity.shoestore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+var bLock = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
@@ -21,13 +22,25 @@ class MainActivity : AppCompatActivity() {
         //val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         val navController =this.findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
-        //NavigationUI.setupWithNavController(binding., navController)
+
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, args: Bundle? ->
+            if (nd.id == R.layout.shoelist_fragment) {
+                Timber.i("Navigation to shoeList")
+                bLock= 1
+            } else {
+                Timber.i("Navigation somewhere anywhere")
+                bLock = 0
+            }
+        }
 
 
 
     }
     override fun onSupportNavigateUp(): Boolean {
+        if(bLock == 1){
+            return false
+        }else{
         val navController = this.findNavController(R.id.myNavHostFragment)
         return navController.navigateUp()
-    }
+    }}
 }
